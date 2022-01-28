d12f50d1e939153a5dfbabb28afa88015489f333
João V. Veras <joaovitorzv@outlook.com>
2022-01-28 17:01:15 -0300

    feat: TabsWrapper component

---
+++ src/TabsWrapper.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+function Tabs() {
+  return <div className="tabs-wrapper">{React.Children}</div>;
+}
+
+export default Tabs;
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,3 @@
-import * as React from 'react';
+import Tabs from './TabsWrapper';
 
-// Delete me
-export const Thing = () => {
-  return <div>this lib suck</div>;
-};
+export { Tabs };
