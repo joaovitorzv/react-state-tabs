f156abd4d44c7f108a160c775de25fbd77f14fd5
João V. Veras <joaovitorzv@outlook.com>
2022-02-07 15:11:04 -0300

    feat: animated tab transition cursor

--- src/Tabs.tsx
+++ src/Tabs.tsx
@@ -9,8 +9,32 @@ interface ITabs {
   defaultActiveTab?: number;
 }
 
-function Tabs({ children, defaultActiveTab }: ITabs) {
+interface ITabProperties {
+  width: number;
+  startPos: number;
+}
+
+function Tabs({ children, defaultActiveTab = 1 }: ITabs) {
   const [active, setActive] = React.useState(defaultActiveTab);
+  const [tabProperties, setTabProperties] = React.useState<ITabProperties>({
+    width: 0,
+    startPos: 0,
+  });
+
+  const navigationRef = React.useRef<HTMLElement>(null);
+  const tabRef = React.useRef<HTMLLIElement>(null);
+
+  React.useEffect(() => {
+    if (!tabRef.current || !navigationRef.current) return;
+
+    setTabProperties({
+      startPos:
+        tabRef.current.getBoundingClientRect().left -
+        navigationRef.current.getBoundingClientRect().left,
+      width: tabRef.current.clientWidth,
+    });
+    console.log(tabProperties);
+  }, [tabRef, active]);
 
   function handleTabMousePress(id: number, disabled?: boolean) {
     if (disabled) return;
@@ -31,34 +55,46 @@ function Tabs({ children, defaultActiveTab }: ITabs) {
 
   return (
     <div className="tabs-wrapper">
-      <ul data-testid="tabs-navigation">
-        {children.map(tab => (
-          <li
-            key={tab.props.id}
-            className={clsx(
-              'tab-nav',
-              {
-                'tab-nav--active': tab.props.id === active,
-              },
-              {
-                'tab-nav--disabled': tab.props.disabled,
+      <nav className="navigation" ref={navigationRef}>
+        <ul data-testid="tabs-navigation" className="container-nav">
+          {children.map(tab => (
+            <li
+              ref={tab.props.id === active ? tabRef : undefined}
+              key={tab.props.id}
+              className={clsx(
+                'tab-nav',
+                {
+                  'tab-nav--active': tab.props.id === active,
+                },
+                {
+                  'tab-nav--disabled': tab.props.disabled,
+                }
+              )}
+              onClick={() =>
+                handleTabMousePress(tab.props.id, tab.props.disabled)
               }
-            )}
-            onClick={() =>
-              handleTabMousePress(tab.props.id, tab.props.disabled)
-            }
-            onKeyDown={e =>
-              handleTabKeyPress(e, tab.props.id, tab.props.disabled)
-            }
-            role="button"
-            aria-pressed={tab.props.id === active}
-            aria-disabled={tab.props.disabled}
-            tabIndex={0}
-          >
-            {tab.props.tabName}
-          </li>
-        ))}
-      </ul>
+              onKeyDown={e =>
+                handleTabKeyPress(e, tab.props.id, tab.props.disabled)
+              }
+              role="button"
+              aria-pressed={tab.props.id === active}
+              aria-disabled={tab.props.disabled}
+              tabIndex={tab.props.id === active ? 0 : -1}
+            >
+              {tab.props.tabName}
+            </li>
+          ))}
+        </ul>
+        <span
+          className="cursor"
+          style={{
+            height: '2px',
+            position: 'relative',
+            left: tabProperties.startPos,
+            width: tabProperties.width,
+          }}
+        />
+      </nav>
       {children.map(tab => (
         <div
           data-testid={`tabContent-${tab.props.id}`}
